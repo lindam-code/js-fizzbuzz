c697b2a77e629115fec0efbd1b8fc776ed817840
lindam <linda.minotti2@gmail.com>
2020-05-27 19:31:39 +0200

    FizBuzz migliorato: variabile numero interna al ciclo.

--- js/script.js
+++ js/script.js
@@ -3,20 +3,32 @@
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 // Dichiarazione variabili
-var numero = 0;
+// var numero = 0;
 var listaFizzBuzz = document.getElementById('lista_fizzbuzz');
 
 // Ciclo for per scrivere la lista di numeri da 1 a 100
-for (var i = 1; i <= 100; i++) {
-  numero += 1;
-  // Condizione per i multipli sia di tre che di 5 'FizzBuzz'
-  if (numero % 3 == 0 && numero % 5 == 0) {
-    numero = 'FizzBuzz';
-  } else if (numero % 3 == 0) { // Condizione per i multipli di tre 'Fizz'
-    numero = 'Fizz';
-  } else if (numero % 5 == 0) {  // Condizione per i multipli di 5 'Buzz'
-    numero = 'Buzz';
-  }
+// for (var i = 1; i <= 100; i++) {
+//   numero += 1;
+//   // Condizione per i multipli sia di tre che di 5 'FizzBuzz'
+//   if (numero % 3 == 0 && numero % 5 == 0) {
+//     numero = 'FizzBuzz';
+//   } else if (numero % 3 == 0) { // Condizione per i multipli di tre 'Fizz'
+//     numero = 'Fizz';
+//   } else if (numero % 5 == 0) {  // Condizione per i multipli di 5 'Buzz'
+//     numero = 'Buzz';
+//   }
+
+  for (var i = 1; i <= 100; i++) {
+    var numero = i;
+
+    // Condizione per i multipli sia di tre che di 5 'FizzBuzz'
+    if (numero % 3 == 0 && numero % 5 == 0) {
+      numero = 'FizzBuzz';
+    } else if (numero % 3 == 0) { // Condizione per i multipli di tre 'Fizz'
+      numero = 'Fizz';
+    } else if (numero % 5 == 0) {  // Condizione per i multipli di 5 'Buzz'
+      numero = 'Buzz';
+    }
+ // Stampa a lista
   listaFizzBuzz.innerHTML +=  '<li>' + numero + '</li>';
-  numero = i;
 }
