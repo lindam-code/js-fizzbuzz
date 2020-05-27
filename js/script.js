289f6cac6be7d21b08e6baadb2415864cfe58019
lindam <linda.minotti2@gmail.com>
2020-05-27 15:37:57 +0200

    FizzBuzz completato.

--- js/script.js
+++ js/script.js
@@ -9,8 +9,10 @@ var listaFizzBuzz = document.getElementById('lista_fizzbuzz');
 // Ciclo for per scrivere la lista di numeri da 1 a 100
 for (var i = 1; i <= 100; i++) {
   numero += 1;
-  // Condizione per i multipli di tre 'Fizz'
-  if (numero % 3 == 0) {
+  // Condizione per i multipli sia di tre che di 5 'FizzBuzz'
+  if (numero % 3 == 0 && numero % 5 == 0) {
+    numero = 'FizzBuzz';
+  } else if (numero % 3 == 0) { // Condizione per i multipli di tre 'Fizz'
     numero = 'Fizz';
   } else if (numero % 5 == 0) {  // Condizione per i multipli di 5 'Buzz'
     numero = 'Buzz';
@@ -18,8 +20,3 @@ for (var i = 1; i <= 100; i++) {
   listaFizzBuzz.innerHTML +=  '<li>' + numero + '</li>';
   numero = i;
 }
-
-
-// Condizione per i multipli di 5
-
-// Condizione per i numeri che sono sia multipli di tre che di 5
